060fcbde1d7f97d89854f01bbe01cf1454bd1d93
skandau <73341866+skandau@users.noreply.github.com>
2022-08-10 13:19:11 -0700

    Add files via upload
    changed to MIT License

--- sloconv.c
+++ sloconv.c
@@ -5,26 +5,31 @@ Command line tool to convert between png <> SLO format
 Requires:
 	-"stb_image.h" (https://github.com/nothings/stb/blob/master/stb_image.h)
 	-"stb_image_write.h" (https://github.com/nothings/stb/blob/master/stb_image_write.h)
-	-"SLO.h" (https://github.com/phoboslab/SLO/blob/master/SLO.h)
+	-"SLO.h" (https://github.com/skandau/SLO.h)
 
 Compile with: 
 	gcc SLOconv.c -std=c99 -O3 -o SLOconv
 
--- LICENSE: GPL License
+-- LICENSE: MIT License
 
 Based on QOI Copyright(c) 2021 Dominic Szablewski
 SLO release 2022 surya kandau
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details at
-    Visit <http://www.gnu.org/copyleft/gpl.html>.
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files(the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions :
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 */
 
@@ -47,8 +52,8 @@ int main(int argc, char **argv) {
 	if (argc < 3) {
 		puts("Usage: SLOconv <infile> <outfile>");
 		puts("Examples:");
-		puts("  SLOconv input.png output.SLO");
-		puts("  SLOconv input.SLO output.png");
+		puts("  SLOconv input.png output.slo");
+		puts("  SLOconv input.slo output.png");
 		exit(1);
 	}
 
